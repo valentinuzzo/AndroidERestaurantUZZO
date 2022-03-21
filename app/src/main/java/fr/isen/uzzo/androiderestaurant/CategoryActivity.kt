af9ed397b91da33b7f3dbff37e88b560162fa3b5
valentinuzzo <valentin.uzzo@isen.yncrea.fr>
2022-03-21 18:28:28 +0100

    commit fin 21/03

--- app/src/main/java/fr/isen/uzzo/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/uzzo/androiderestaurant/CategoryActivity.kt
@@ -3,9 +3,9 @@ package fr.isen.uzzo.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_category)
     }
 }
--- app/src/main/java/fr/isen/uzzo/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/uzzo/androiderestaurant/HomeActivity.kt
@@ -1,4 +1,47 @@
 package fr.isen.uzzo.androiderestaurant
 
-class HomeActivity {
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import fr.isen.uzzo.androiderestaurant.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding   //declaration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)   //initialisation
+        setContentView(binding.root)    //utilisation
+
+        binding.BoutonEntrees.setOnClickListener{
+            Toast.makeText(
+                this@HomeActivity,
+                "Redirection vers la page d'accueil",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        binding.BoutonEntrees.setOnClickListener{
+           goToCategory(getString(R.string.home_starters))
+        }
+
+        binding.BoutonPlats.setOnClickListener{
+            goToCategory(getString(R.string.home_dish))
+        }
+
+        binding.BoutonDesserts.setOnClickListener{
+            goToCategory(getString(R.string.home_desert))
+        }
+
+    }
+
+    private fun goToCategory(category: String) {
+        val intent = Intent(this, CategoryActivity::class.java)
+        intent.putExtra("category", category)
+        startActivity(intent)
+    }
 }
+
